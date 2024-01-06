4d2904bde82ecfc3aa237766eea3b3c129a06f71
devgiljong <calf-love@naver.com>
2024-01-06 20:39:11 +0900

    :sparkles: Router

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+interface indexProps {}
+
+const index = ({}: indexProps) => {
+  return (
+    <View>
+      <Text>dsagsdi</Text>
+    </View>
+  );
+};
+
+export default index;
