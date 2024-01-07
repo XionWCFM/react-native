51d10a3ba432307b1b9a9d6c5f4117fd2e501219
devgiljong <calf-love@naver.com>
2024-01-07 17:21:55 +0900

    :sparkles: Icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-
+import { FontAwesome } from '@expo/vector-icons';
 const TabsLayout = () => {
   return (
     <Tabs>
@@ -8,6 +8,7 @@ const TabsLayout = () => {
         options={{
           headerTitle: 'home page',
           title: 'Home',
+          tabBarIcon: (props) => <FontAwesome name={'home'} {...props} />,
         }}
       />
       <Tabs.Screen
@@ -15,6 +16,7 @@ const TabsLayout = () => {
         options={{
           headerTitle: 'user page',
           title: 'user',
+          tabBarIcon: (props) => <FontAwesome name={'user'} {...props} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,29 @@
 import { Link, router } from 'expo-router';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import IonIcons from '@expo/vector-icons/Ionicons';
 
 interface indexProps {}
+
+const ButtonContainer = styled.TouchableOpacity`
+  width: 120px;
+  height: 40px;
+  padding: 12px;
+  border-radius: 10px;
+`;
+const ButtonText = styled.Text`
+  font-size: 16px;
+  text-align: center;
+`;
 
 const index = ({}: indexProps) => {
   return (
     <View>
+      <IonIcons name={'md-checkmark'} size={32} color={'green'} />
+      <ButtonContainer>
+        <ButtonText>안녕하세요</ButtonText>
+      </ButtonContainer>
       <Text>dsagsdi</Text>
       <Link href={'/users/1'}>이동하기</Link>
       <Pressable onPress={() => router.push('/users/2')}>
