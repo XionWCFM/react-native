f82fa26e4c238091395b156189f3e50aae2891cc
devgiljong <calf-love@naver.com>
2024-01-07 15:18:55 +0900

    :sparkles: 헤더표시안하기

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,13 @@ import { Stack } from 'expo-router';
 const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          //header 표시 안하기
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 };
