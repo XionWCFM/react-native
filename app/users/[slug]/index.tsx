acd20803befa189718fb09b8d76a911a998f2538
devgiljong <calf-love@naver.com>
2024-01-07 14:45:43 +0900

    :sparkles: Add

--- app/users/[slug]/index.tsx
+++ app/users/[slug]/index.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 interface indexProps {}
 
 const index = ({}: indexProps) => {
+  const { slug } = useLocalSearchParams();
   return (
     <View>
-      <Text>hfklgmklsdml</Text>
+      <Text>{slug} 슬러그는 이렇습니다.</Text>
       <Link href={'/'}>홈으로돌아가기</Link>
     </View>
   );
