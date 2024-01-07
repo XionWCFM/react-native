b711f920446178904bf0215c8bbb36d0341f3d17
devgiljong <calf-love@naver.com>
2024-01-07 14:43:32 +0900

    :sparkles: Add

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 interface indexProps {}
 
@@ -9,6 +9,9 @@ const index = ({}: indexProps) => {
     <View>
       <Text>dsagsdi</Text>
       <Link href={'/users/1'}>이동하기</Link>
+      <Pressable onPress={() => router.push('/users/2')}>
+        <Text>Go to user2</Text>
+      </Pressable>
     </View>
   );
 };
