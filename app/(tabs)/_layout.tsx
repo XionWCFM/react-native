ab10411aa12c3e60d19ccd6d44133b24c9efd39f
devgiljong <calf-love@naver.com>
2024-01-07 16:16:38 +0900

    :sparkles: Layout tㅜ정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,18 @@ import { Tabs } from 'expo-router';
 const TabsLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="users/[slug]" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: 'home page',
+        }}
+      />
+      <Tabs.Screen
+        name="users/[slug]"
+        options={{
+          headerTitle: 'user page',
+        }}
+      />
     </Tabs>
   );
 };
