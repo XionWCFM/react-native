a1d466b43abfe9c3b5bc61e126abc5c386791b3f
devgiljong <calf-love@naver.com>
2024-01-07 15:12:26 +0900

    :sparkles: Ad
    d

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,21 +3,7 @@ import { Stack } from 'expo-router';
 const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen
-        name={'index'}
-        options={{
-          headerTitle: 'Home page',
-          headerStyle: {
-            backgroundColor: 'red',
-          },
-        }}
-      />
-      <Stack.Screen
-        name={'users/[slug]/index'}
-        options={{
-          headerTitle: 'users page',
-        }}
-      />
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 };
